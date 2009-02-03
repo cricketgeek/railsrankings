df99041681b44577e899c332570dd99cf9aab7c2
Cricketgeek <jonesmc123@gmail.com>
2009-02-02 19:26:38 -0500

    added hoptoad error support, free signup for now

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -8,6 +8,9 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'd59b651ef4ce6957caee0dfc59f326a3'
   
+  include HoptoadNotifier::Catcher
+  
+  
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,3 @@
+HoptoadNotifier.configure do |config|
+  config.api_key = 'c26a8470dc8d9e47ca73406125e3de20'
+end
